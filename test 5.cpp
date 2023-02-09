d295fd9b1969b9e091c78e2e239c3f3d752dd106
AmirxonBahromov <124458723+AmirxonBahromov@users.noreply.github.com>
2023-02-09 11:45:56 +0500

    Add files via upload

---
+++ test 5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int digit = 0;
+	double digit2 = 0;
+	char symbol = 0;
+	bool trueOrFalse = 0;
+	cout << "yirik sonni kiriting ";
+	cin >> digit;
+	cout << "sonni kiriting ";
+	cin >> digit2;
+	cout << "simvolni kiriting ";
+	cin >> symbol;
+	cout << "1 yoki nolni kiriting ";
+	cin >> trueOrFalse;
+	cout << endl << endl;
+	cout << "butun son " << digit << endl;
+	cout << "yirik bolmagan son kiriting " << digit2 << endl;
+	cout << "simvol " << symbol << endl;
+	cout << "bool " << trueOrFalse << endl;
+	return 0;
+}
