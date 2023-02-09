89fa14ef373ab1604a74eff539b4b77da77b852e
AmirxonBahromov <124458723+AmirxonBahromov@users.noreply.github.com>
2023-02-09 11:43:06 +0500

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int magic;//
+	int tahmin;//
+	magic = rand();//
+	cout << "tanlagan soningizni kiriting";
+	cin >> tahmin;
+	if (tahmin == magic) cout << "togri";
+	else cout << "notogri";
+	return 0;
+}
