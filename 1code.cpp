b112dda4d0b5ff9cd770b9508aa1667e562e1a54
AmirxonBahromov <124458723+AmirxonBahromov@users.noreply.github.com>
2023-02-09 11:42:31 +0500

    Add files via upload

---
+++ 1code.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int magic;
+	int keyingi;
+	magic = rand();
+	cout << "oylagan sonni yozing";
+	cin >> keyingi;
+	if (keyingi ==magic)
+		cout << "togri";
+	else
+		cout << "notogri";
+	return 0;
+}
+
+
+ 
